4193d6480c63d5d9fab1fd127412e68915951714
neoribello <neil.oribello@gmail.com>
2021-06-09 00:19:44 -0700

    added dropdown for filter

--- src/components/MovieList/MovieFilter.js
+++ src/components/MovieList/MovieFilter.js
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 const api_key  = "e68f0e35dcc5a1bd27bfaedc41d3c894";
 const BASE_URL = "https://api.themoviedb.org/3";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function MovieFilter(props) {
   const [data, setData] = useState([]);
@@ -17,12 +33,36 @@ function MovieFilter(props) {
     });
   }, []);
 
+  const classes = useStyles();
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <div>
       <div className="movielist-filter">
         <div classname="filter-container">
           <p>Movie Filter</p>
         </div>
+
+        <FormControl variant="filled" className={classes.formControl}>
+        <InputLabel id="demo-simple-select-filled-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-filled-label"
+          id="demo-simple-select-filled"
+          value={age}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
       </div>
     </div>
   );
